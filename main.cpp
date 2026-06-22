d30bf718bf585e48f64d76a3eefdd8e40c9043f4
91325830Kj <caiostuart06@gmail.com>
2026-06-22 20:59:29 -0300

    Insertion Sort

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+void insertionSort(vector<int>& v);
+
 int main() {
     int M, N, X;
 
@@ -19,19 +21,16 @@ int main() {
 
     vector<vector<int>> vetores(M, vector<int>(N));
 
-    // Melhor caso (ordenado)
     if (M >= 1) {
         for (int i = 0; i < N; i++)
             vetores[0][i] = i + 1;
     }
 
-    // Pior caso (ordem inversa)
     if (M >= 2) {
         for (int i = 0; i < N; i++)
             vetores[1][i] = N - i;
     }
 
-    // Vetores aleatórios
     srand(time(NULL));
 
     for (int i = 2; i < M; i++) {
@@ -40,14 +39,41 @@ int main() {
         }
     }
 
-    // Exibir vetores
+    cout << "\nAntes da ordenacao:\n";
     for (int i = 0; i < M; i++) {
-        cout << "\nVetor " << i + 1 << ": ";
-
         for (int j = 0; j < N; j++) {
             cout << vetores[i][j] << " ";
         }
+        cout << endl;
+    }
+
+    for (int i = 0; i < M; i++) {
+        insertionSort(vetores[i]);
+    }
+
+    cout << "\nDepois da ordenacao:\n";
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << vetores[i][j] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
+}
+
+void insertionSort(vector<int>& v) {
+    int n = v.size();
+
+    for (int i = 1; i < n; i++) {
+        int chave = v[i];
+        int j = i - 1;
+
+        while (j >= 0 && v[j] > chave) {
+            v[j + 1] = v[j];
+            j--;
+        }
+
+        v[j + 1] = chave;
+    }
 }
